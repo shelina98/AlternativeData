fc5307e975bbeebc2c9c06a940479b2b51930686
Shela Veliu <shelaveliu1@gmail.com>
2022-01-16 15:08:04 +0100

    CREATE COMPANY API WITH CSV FILE

---
+++ companies/CSV/company_csv.py
@@ -0,0 +1,10 @@
+import csv
+import pandas as pd
+
+with open('companies_sample.csv', 'r') as companies_csv:
+    csv_reader = csv.reader(companies_csv)
+
+
+   with open()
+    for line in csv_reader:
+        print(line)
